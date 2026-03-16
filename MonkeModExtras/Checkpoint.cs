ac75a954f9a190bd830569cc204caafcb438577f
Estati <skeppyfan290@gmail.com>
2026-03-15 19:10:34 -0500

    forgot to commit these

---
+++ MonkeModExtras/Checkpoint.cs
@@ -0,0 +1,58 @@
+﻿using Monke_Mod_Panel.Attributes;
+using UnityEngine;
+using GorillaLibrary.Utilities;
+
+namespace ModTemplate;
+
+[Toggleable]
+public class Checkpoint : Monke_Mod_Panel.Mod
+{
+    public override string Name => "CheckPoint";
+    private GameObject checkpointer;
+    public float maxDistance = 100f;
+    public bool enablayd;
+
+    public override void OnEnable()
+    {
+        enablayd = true;
+        checkpointer = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        checkpointer.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        checkpointer.GetComponent<Renderer>().material = new Material(Shader.Find("GorillaTag/UberShader"));
+        checkpointer.GetComponent<Renderer>().material.color = Color.purple;
+        checkpointer.GetComponent<Collider>().Destroy();
+        
+    }
+
+    public override void OnDisable()
+    {
+        enablayd = false;
+        checkpointer.Destroy();
+    }
+    public override void OnModdedJoin() {}
+    public override void OnModdedLeave() { OnDisable(); }
+
+
+    public override void OnUpdate()
+    {
+        if (enablayd)
+        {
+            if (InputUtility.RightSecondary.pressed)
+            {
+                GorillaLocomotion.GTPlayer.Instance.TeleportTo(checkpointer.transform.position, Quaternion.identity);
+                checkpointer.GetComponent<Renderer>().material.color = Color.green;
+            }
+
+            if (InputUtility.RightGrip.GetValue() > 0.7f && !InputUtility.RightSecondary.pressed)
+            {
+
+                checkpointer.transform.position = GorillaTagger.Instance.rightHandTransform.position;
+                checkpointer.GetComponent<Renderer>().material.color = Color.purple;
+            }
+            else if (!InputUtility.RightSecondary.pressed)
+
+            {
+                checkpointer.GetComponent<Renderer>().material.color = Color.blue;
+            }
+        }
+    }
+}
