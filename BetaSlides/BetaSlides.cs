de0291438e24c7833152ad79b88f596a166d5bd3
Estati <skeppyfan290@gmail.com>
2026-03-26 23:34:47 -0500

    added slipping and tap sounds

--- BetaSlides/BetaSlides.cs
+++ BetaSlides/BetaSlides.cs
@@ -17,22 +17,35 @@ public class BetaSlides : Monke_Mod_Panel.Mod
     public override string Name => "Beta Slides";
     public bool enabled = false;
     public GameObject slides;
-
+    public GameObject slide1 = null;
+    public GameObject slide2 = null;
+    public GameObject slide3 = null;
+    public GameObject slidet1 = null;
+    public GameObject slidet2 = null;
+    public GameObject slidet3 = null;
     public override void OnModdedJoin()
     {
         Bundle = AssetBundleUtility.LoadBundle(System.Reflection.Assembly.GetExecutingAssembly(),
             "ModTemplate.Resources.talkingslidethatdog");
         slides = Object.Instantiate(Bundle.LoadAsset<GameObject>("messy"));
         slides.transform.position = new Vector3(-67.0938f, 11.7505f, -82.6406f);
-        MelonLogger.Msg("this WORKED on load bundle");
-        if (!enabled)
-        {
+        
+        slide1 = GameObject.Find("bigslidespiralslide");
+        slide2 = GameObject.Find("bigslideslide");
+        slide3 = GameObject.Find("bigslideslide2");
+        slide1.AddComponent<GorillaSurfaceOverride>().overrideIndex = 59;
+        slide2.AddComponent<GorillaSurfaceOverride>().overrideIndex = 59;
+        slide3.AddComponent<GorillaSurfaceOverride>().overrideIndex = 59;
+        slidet1 = GameObject.Find("bigslidestructurespiral");
+        slidet2 = GameObject.Find("bigslidestructure");
+        slidet3 = GameObject.Find("bigslidestructure2");
+        slidet1.AddComponent<GorillaSurfaceOverride>().overrideIndex = 26;
+        slidet2.AddComponent<GorillaSurfaceOverride>().overrideIndex = 26;
+        slidet3.AddComponent<GorillaSurfaceOverride>().overrideIndex = 26;
             slides.SetActive(false);
-        }
-        else
-        {
-            slides.SetActive(true);
-        }
+        MelonLogger.Msg("this WORKED on load bundle");
+
+
     }
 
     public override void OnEnable()
